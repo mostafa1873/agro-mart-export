13cb9e6650236c28d49ba25e5cf7f5c63d74c596
mostafa1873 <os8722398@gmail.com>
2026-02-01 21:00:32 +0200

    update 1

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+// import viteImagemin from 'vite-plugin-imagemin';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),
-    tailwindcss(),
+  tailwindcss(),
+  // viteImagemin({
+  //   gifsicle: { optimizationLevel: 7 },
+  //   optipng: { optimizationLevel: 7 },
+  //   mozjpeg: { quality: 20 },
+  //   pngquant: { quality: [0.8, 0.9], speed: 4 },
+  //   svgo: { plugins: [{ removeViewBox: false }] },
+  // }),
   ],
 })
